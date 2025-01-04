2289f1967282aa17e3ad04a8ed339a16ce2a943d
Endrika Nanami <endrika.nanami@gmail.com>
2025-01-04 09:17:09 +0700

    Remove element plus, i think im gonna use vanilla css for this project

--- src/plugins/element-plus.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Element Plus UI Library
- *
- */
-
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-export default ElementPlus
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -6,8 +6,7 @@
 import type { App } from 'vue'
 import router from '@/router'
 import pinia from '@/plugins/pinia.ts'
-import elementPlus from '@/plugins/element-plus'
 
 export function registerPlugins(app: App) {
-  app.use(router).use(pinia).use(elementPlus)
+  app.use(router).use(pinia)
 }
